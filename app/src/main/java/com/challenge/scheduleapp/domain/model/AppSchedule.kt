59ac3b83ff9332b82d71094029d16ed59c7a0236
Imtiaz <imtiazkalamabir@gmail.com>
2025-10-23 02:00:40 +0600

    Added viewbinding library Created and initialized views in ScheduleListActivity Created views for list items Created ScheduleListAdapter, AppListAdapter, ScheduleViewModel Created data class AppSchedule Added necessary strings

---
+++ app/src/main/java/com/challenge/scheduleapp/domain/model/AppSchedule.kt
@@ -0,0 +1,12 @@
+package com.challenge.scheduleapp.domain.model
+
+data class AppSchedule(
+    val id: Long = 0,
+    val packageName: String,
+    val appName: String,
+    val appIconPath: String? = null,
+    val scheduledTime: Long,
+    val status: ScheduleStatus,
+    val createdAt: Long,
+    val executedAt: Long? = null
+)
---
+++ app/src/main/java/com/challenge/scheduleapp/presentation/viewmodel/ScheduleViewModel.kt
@@ -0,0 +1,9 @@
+package com.challenge.scheduleapp.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ScheduleViewModel @Inject constructor(): ViewModel() {
+}
